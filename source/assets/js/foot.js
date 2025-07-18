daf103ea2367b452aef53a4007f671178336aeea
dr34m-cn <1416814478@qq.com>
2025-07-18 09:32:10 +0800

    update 更新底部时间，显示到年

--- source/assets/js/foot.js
+++ source/assets/js/foot.js
@@ -14,24 +14,27 @@
     function update() {
         var now = new Date();
         now.setTime(now.getTime() + 250);
-        days = (now - start) / 1000 / 60 / 60 / 24;
+		ns = Math.floor((now - start) / 1000);
+		years = ns / 60 / 60 / 24 / 365;
+		ynum = Math.floor(years);
+        days = ns / 60 / 60 / 24 % 365;
         dnum = Math.floor(days);
-        hours = (now - start) / 1000 / 60 / 60 - (24 * dnum);
+        hours = ns / 60 / 60 % 24;
         hnum = Math.floor(hours);
         if (String(hnum).length === 1) {
             hnum = "0" + hnum;
         }
-        minutes = (now - start) / 1000 / 60 - (24 * 60 * dnum) - (60 * hnum);
+        minutes = ns / 60 % 60;
         mnum = Math.floor(minutes);
         if (String(mnum).length === 1) {
             mnum = "0" + mnum;
         }
-        seconds = (now - start) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
-        snum = Math.round(seconds);
+        seconds = ns % 60;
+        snum = Math.floor(seconds);
         if (String(snum).length === 1) {
             snum = "0" + snum;
         }
-        document.getElementById("timeDate").innerHTML = "本站安全运行&nbsp" + dnum + "&nbsp天";
+        document.getElementById("timeDate").innerHTML = "本站安全运行&nbsp" + ynum + "&nbsp年&nbsp" + dnum + "&nbsp天";
         document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
     }
 
